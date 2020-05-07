d7bf91007119fce46c87cf1f27d16711b61356a5
vaishnavi <vaishnavip@prdxn.com>
2020-05-07 11:14:17 +0530

    function written to display values in list,modify list and remove item from list

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,43 @@ function store(name,quan){
     display();
 }
 
+/*function to display user-input*/
+function display(){
+    var itemname=localStorage.getItem("ItemName");
+    var qvalue=localStorage.getItem("Quantity");
+    var liststruc = "<span>"+itemname+"</span><span>"+qvalue+"</span><a href='#FIXME' onclick='editlist(this)' title='Modify'>Modify</a><a href='#FIXME' onclick='remove(this)' title='Remove'>Remove</a>";
+    var li=document.createElement("Li");
+    li.innerHTML=liststruc;
+    document.getElementById("userlist").appendChild(li);
+}
+
+savebtn.addEventListener("click",getVal,false);
+
+/*function to edit list*/
+function editlist(li){
+   selectedLi=li.parentElement;
+   span= selectedLi.querySelectorAll('span');
+   document.getElementById("item").value=span[0].innerHTML;
+   document.getElementById("quant").value=span[1].innerHTML;
+   return span;
+}
+
+/*function to update list*/
+function modify(name,quan){
+    span[0].innerHTML=name;
+    span[1].innerHTML=quan;
+}
+
+/*function to remove element from list*/
+function remove(li){
+    if(confirm("Are you sure you want to delete?")){
+        listrow=li.parentElement;
+        localStorage.removeItem("ItemName");
+        localStorage.removeItem("Quantity");
+        listrow.remove();
+    }
+}
+
 
 
 
