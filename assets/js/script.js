8790d9984818c119b3fb6afbdb41313464c6e32b
vaishnavi <vaishnavip@prdxn.com>
2020-05-07 11:12:02 +0530

    function written to store value in localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
 /* Author: 
 
 */
+var savebtn=document.getElementById("saved");
 
+/* function to get value from user*/ 
+var selectedLi=null;
+function getVal(){
+    var name=document.getElementById("item").value;
+    var quan=document.getElementById("quant").value;
+    if(selectedLi == null){
+        store(name,quan);
+    }
+    else{
+        modify(name,quan);
+    }
+    document.getElementById("item").value="";
+    document.getElementById("quant").value="";
+}
+
+/*function to store values  in local storage*/
+function store(name,quan){
+    localStorage.setItem("ItemName",name);
+    localStorage.setItem("Quantity",quan);
+    display();
+}
 
 
 
